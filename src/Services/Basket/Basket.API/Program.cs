29b1d6194c4e832ba9ba7ef2a325f9f7aea62eb4
Sarathi Prabu <sarathiprabum@gmail.com>
2025-12-17 11:42:46 -0500

    Add GetBasket endpoint - Implement GetBasketEndpoints.cs and GetBasketHandler.cs - Added BuildingBlocks reference to this project - Injected Carter, MediatR, Validation behavior and Logging Behavior to DI container

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+
+var app = builder.Build();
+app.MapCarter();
 
 app.Run();
