7240cf9cbb3ab78f20cfde9411ed557548cd3995
Sarathi Prabu <sarathiprabum@gmail.com>
2025-12-17 22:45:02 -0500

    Add Store and Delete Basket - Implement endpoits and handlers for Delete and Store Basket - Refactor global using - update launch browse false

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,24 @@
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart): ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart is required");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("User Name is required");
+    }
+}
+public class StoreBasketCommandHandler: ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        // TODO
+        // Store Basket in the DB (Upsert)
+        // Update cache
+        return new StoreBasketResult("User1");
+    }
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+global using Basket.API.Model;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using BuildingBlocks.Behaviors;
+global using Mapster;
+global using FluentValidation;
