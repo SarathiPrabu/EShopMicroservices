55ff809cc0bd62d09c1d90502ae9d05e5af9a180
Sarathi Prabu <sarathiprabum@gmail.com>
2025-12-21 02:11:52 -0500

    Add cached basket repository with Redis
    - Added CachedBasketRepository as a decorator for BasketRepository
    - Implemented distributed caching for Basket API
    - Integrated Redis for cache storage
    - Added Scrutor to support decorator pattern

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -13,12 +13,24 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+// builder.Services.AddScoped<IBasketRepository>(provider =>
+// {
+//     var basketRepository = provider.GetRequiredService<BasketRepository>();
+//     return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+// });
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 var app = builder.Build();
 
 // Configure HTTP request pipeline
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+
 app.Run();
