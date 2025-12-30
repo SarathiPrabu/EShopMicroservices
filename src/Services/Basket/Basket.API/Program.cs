45e29fbbf7664b3a413a2c25750b36f645d28eba
Sarathi Prabu <sarathiprabum@gmail.com>
2025-12-29 20:54:27 -0500

    Add Discount gRPC reference in Basket.Api - Added DiscountUrl in appsettings.json - Added discount grpc reference as client - Added Grpc client NuGet Packages - Added gRPC service to the DI container - Added exception to accept any connection - Added DeductDiscount mentod in StoreBasketHandler.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,11 @@
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
+
+// ------- Application Services ------- //
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
@@ -11,25 +14,37 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+// ------- Data Services ------- //
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
-// builder.Services.AddScoped<IBasketRepository>(provider =>
-// {
-//     var basketRepository = provider.GetRequiredService<BasketRepository>();
-//     return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
-// });
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
+
+// ------- gRPC Services ------- //
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});;
+// ------- Cross-cutting Services ------- //
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
