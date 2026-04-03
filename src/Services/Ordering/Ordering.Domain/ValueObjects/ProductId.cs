8788a7459821f247b22414878c4d8db9e730ab84
Sarathi Prabu <sarathiprabum@gmail.com>
2026-04-03 16:34:19 -0400

    Add validations for value objects
    - Implemented domain-level validations in value objects
    - Introduced global using for domain exception handling

--- src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/ProductId.cs
@@ -1,6 +1,16 @@
 namespace Ordering.Domain.ValueObjects;
+public record ProductId
+{
+    public Guid Value { get; }
+    private ProductId(Guid value) => Value = value;
+    public static ProductId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("ProductId cannot be empty.");
+        }
 
-public record ProductId()
-{ 
-    public Guid Value { get;}
+        return new ProductId(value);
+    }
 }
