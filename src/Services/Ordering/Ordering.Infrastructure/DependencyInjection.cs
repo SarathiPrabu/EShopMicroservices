ec792b8738ef51dfe0dec855214e004d2671542a
Sarathi Prabu <sarathiprabum@gmail.com>
2026-04-09 14:00:40 -0400

    Refactor Dependency Injection setup
    - Organized DI configuration into dedicated files
    - Imported DI modules in `Program.cs`
    - Improved structure for better maintainability and separation of concerns

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,20 @@
-namespace Ordering.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace Ordering.Infrastructure;
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //// Add services to the container.
+        //services.AddDbContext<ApplicationDbContext>(options =>
+        //    options.UseSqlServer(connectionString));
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
 }
