39832bbc577b4d388047c6e2ab634fa67f99df9f
Sarathi Prabu <sarathiprabum@gmail.com>
2026-04-27 13:52:25 -0400

    Add SavingChanges Interceptors OrderDb

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -10,7 +10,10 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Database");
         //// Add services to the container.
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
         // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         return services;
     }
